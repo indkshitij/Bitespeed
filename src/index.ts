0af0d80856a1ba808d58cfae711377068b36a2e2
indkshitij <ind.kshitijsingh@gmail.com>
2026-02-26 23:56:24 +0530

    Ready for deployment

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("Server running on port 3000");
 });
